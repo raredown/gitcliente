93c6ca50f31aeb1cc6a426062f3a316f656e883f
Rafa-HP\Rafa <raredown@gmail.com>
2016-09-19 20:27:40 +0200

    Pruebas todos

--- codigo.js
+++ codigo.js
@@ -6,3 +6,21 @@ function mensajeDeFuera() {
 function mensajeTres(texto) {
     alert(texto);
 }
+function operacion() {
+  var numeroUno = 0; numeroDos = 0; resultado = 0;
+  numeroUno = prompt ('introduce un numero');
+  numeroDos = prompt ('introduce un numero');
+  resultado = parseInt(numeroUno) + parseInt(numeroDos);
+  alert('el reultado de la suma es: '+resultado)
+
+  numeroUno = prompt ('introduce un numero');
+  numeroDos = prompt ('introduce un numero');
+  resultado = parseInt(numeroUno) - parseInt(numeroDos);
+  alert('el reultado de la resta es: '+resultado)
+
+  numeroUno = prompt ('introduce un numero');
+  numeroDos = prompt ('introduce un numero');
+  resultado = parseInt(numeroUno) / parseInt(numeroDos);
+  alert('el reultado de la division es: '+resultado)
+
+}
